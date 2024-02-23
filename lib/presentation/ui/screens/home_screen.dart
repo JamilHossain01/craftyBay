989f9917ffe500baaea924599b5b69b7e9e47f1f
JamilHossain01 <121524109+JamilHossain01@users.noreply.github.com>
2024-02-23 11:28:05 +0600

    home_screen edite

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -22,13 +22,13 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               searchTextField(),
               CarouselSlider(
-                options: CarouselOptions(height: 400.0),
+                options: CarouselOptions(height: 180.0),
                 items: [1, 2, 3, 4, 5].map((i) {
                   return Builder(
                     builder: (BuildContext context) {
                       return Container(
                           width: MediaQuery.of(context).size.width,
-                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          margin: EdgeInsets.symmetric(horizontal: 1.0),
                           decoration: BoxDecoration(color: Colors.amber),
                           child: Text(
                             'text $i',
